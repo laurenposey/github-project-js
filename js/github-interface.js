464158c7c2dc15fb925e7c3c567c8ad18652e7da
Lauren Posey <mrslaurenposey@gmail.com>
2016-08-05 13:27:35 -0700

    do nothing, but i wrote some jQuery to retrieve the user input

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,5 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  
+  $('form#git-user').submit(function(event) {
+    event.preventDefault();
+    var gitUser = $('#git-user-input').val();
+
+  });
 });
