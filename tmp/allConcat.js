70e885fa7dc1e5a57c6d69231b9b4139c53e1459
Lauren Posey <mrslaurenposey@gmail.com>
2016-08-05 15:53:46 -0700

    display some html and that's about it

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,17 +1,10 @@
 var apiKey = require('./../.env').apiKey;
+var Git = require('./../js/github.js').gitModule;
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      console.log(response);
-      console.log(JSON.stringify(response));
-      // paste into http://jsonprettyprint.com/ for easy to understand methods
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
+  $('form#git-user').submit(function(event) {
+    event.preventDefault();
+    var gitUser = $('#git-user-input').val();
+
   });
 });
